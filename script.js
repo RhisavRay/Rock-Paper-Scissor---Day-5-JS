c30be1706cd7a43308e6fcef0f17cf029423424b
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-09-02 08:42:47 +0530

    user choice set

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const computerChoice = document.getElementById("computer-choice");
-const yourChoice = document.getElementById("your-choice");
-const resultDisplay = document.getElementById("result")
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("your-choice");
+const resultDisplay = document.getElementById("result");
+const choices = document.querySelectorAll('button');
 
-const choices = document.querySelectorAll("button");
-
+let userChoice;
+choices.forEach(choice => choice.addEventListener('click', (e) => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+}))
