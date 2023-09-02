850178c98606d0a25c329969aa847d9a183a8079
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-09-02 06:58:14 +0530

    index.html done

---
+++ script.js
@@ -0,0 +1,6 @@
+const computerChoice = document.getElementById("computer-choice");
+const yourChoice = document.getElementById("your-choice");
+const resultDisplay = document.getElementById("result")
+
+const choices = document.querySelectorAll("button");
+
