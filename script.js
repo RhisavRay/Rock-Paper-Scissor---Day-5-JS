3eae0ad6e4bbe487602d3374bed161ea80a3b197
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-09-02 08:49:07 +0530

    random function generated

--- script.js
+++ script.js
@@ -4,8 +4,26 @@ const resultDisplay = document.getElementById("result");
 const choices = document.querySelectorAll('button');
 
 let userChoice;
+let computerChoice;
 choices.forEach(choice => choice.addEventListener('click', (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice();
+    victoryOrLoss(userChoice, computerChoice);
 }))
+
+const generateComputerChoice = () => {
+    const rand = Math.floor(Math.random() * 3) + 1;
+    if(rand === 1)
+        computerChoice = "Rock";
+    else if(rand === 2)
+        computerChoice = "Paper";
+    else
+        computerChoice = "Scissors";
+
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+const victoryOrLoss = (userChoice, computerChoice) => {
+    
+}
